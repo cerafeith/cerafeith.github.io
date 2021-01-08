23cc53435737a18d4e64f074810cf3f5131c41aa
Sarah Piot <sfg.piot@outlook.com>
2021-01-08 09:50:12 -0600

    Updated resume.

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,13 @@
 const education = [
     {
         instution: "University of Saskatchewan",
+        notes: "Bachelor of Science, Computer Science",
         location: "Saskatoon, SK",
-        time: "Sept. 2015 - Present"
+        time: "Sept. 2015 - Dec. 2020"
     },
     {
         instution: "Walter Murray Collegiate Institute",
+        notes: "Grades 9-12",
         location: "Saskatoon, SK",
         time: "Sept. 2010 - June 2015"
     }
@@ -15,15 +17,21 @@ const education = [
 const experience = [
     {
         institution: "University of Saskatchewan",
-        position: "Ta/Marker",
+        position: "TA/Marker",
         location: "Saskatoon, SK",
-        time: "Sept. 2019 - Present"
+        time: "Sept. 2019 - Dec. 2020"
     },
     {
         institution: "Prairie Ink Restaurant and Bakery",
         position: "Barista",
         location: "Saskatoon, SK",
         time: "Sept. 2016 - Sept. 2019"
+    },
+    {
+        institution: "Shopper's Drug Mart",
+        position: "Photo Lab Technician",
+        location: "Saskatoon, SK",
+        time: "May. 2015 - Aug. 2017"
     }
 ];
 
@@ -33,8 +41,10 @@ for(let i = 0; i < education.length; i++) {
     var row = resume.insertRow();
     var cell = row.insertCell();
     var cell2 = row.insertCell();
+    var cell3 = row.insertCell();
     cell.setAttribute("class", "field");
     var ed = education[i].instution + "<br />" +
+        education[i].notes + "<br />" +
         education[i].location + "<br />" +
         education[i].time + "<br />";
     cell2.innerHTML = ed;
